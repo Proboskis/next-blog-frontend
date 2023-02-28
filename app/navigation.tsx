b843a7bc8e5159bf069483a410f05e846d059471
Proboskis <nikolasurla22@gmail.com>
2023-02-28 23:05:16 +0100

    5. Work on header.

--- app/navigation.tsx
+++ app/navigation.tsx
@@ -9,9 +9,32 @@ export default function Navigation() {
                 <div className="flex relative items-center justify-between h-24
                 bg-opacity-80 bg-clip-padding backdrop-filter-blur">
                     <a href="#" target="_blank" rel="noopener noreferrer"
-                       className={`${BonheurRoyale.variable} font-signature text-5xl text-gray-500 hover:text-gray-700`}>
-                        Proboskis
+                       className={`${BonheurRoyale.variable} font-signature text-5xl text-gray-500`}>
+                        <span className="text-orange-600">P</span>
+                        <span>roboskis</span>
                     </a>
+                    <div className="hidden w-full md:block md:ml-6">
+                        <div className="flex justify-end">
+                            <a href="#" className="block px-4 py-2 mx-1 text-lg font-medium leading-5 text-gray-700
+                            border-l-4 border-transparent transition duration-150 ease-in-out rounded-sm
+                            hover:bg-gray-100 border-solid border-orange-600
+                            focus:outline-none focus:bg-gray-100 5xl:text-3xl bg-gray-100">
+                                Home
+                            </a>
+                            <a href="#" className="block px-4 py-2 mx-1 text-lg font-medium leading-5 text-gray-700
+                            border-l-4 border-transparent transition duration-150 ease-in-out rounded-sm
+                            hover:bg-gray-100
+                            focus:outline-none focus:bg-gray-100 5xl:text-3xl">
+                                About
+                            </a>
+                            <a href="#" className="block px-4 py-2 mx-1 text-lg font-medium leading-5 text-gray-700
+                            border-l-4 border-transparent transition duration-150 ease-in-out rounded-sm
+                            hover:bg-gray-100
+                            focus:outline-none focus:bg-gray-100 5xl:text-3xl">
+                                Contacts
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </nav>
--- app/page.tsx
+++ app/page.tsx
@@ -32,24 +32,24 @@ export default function Home() {
             </div>
             <div className="px-8 py-2">
               <a className="text-base text-gray-500 hover:text-gray-900 font-medium" href="/privacy-policy">
-                Policy
+                Privacy policy
               </a>
             </div>
             <div className="px-8 py-2">
               <a className="text-base text-gray-500 hover:text-gray-900 font-medium" href="/terms-and-conditions">
-                Terms and Conditions
+                Terms &amp; Conditions
               </a>
             </div>
           </div>
           <div className="flex justify-center mt-8 space-x-6">
             <a href="#" target="_blank" rel="noopener noreferrer">
-              <BsTwitter className="w-6 h-6 text-gray-400 hover:text-gray-500" />
+              <BsTwitter className="w-6 h-6 text-orange-600 hover:text-orange-400" />
             </a>
             <a href="#" target="_blank" rel="noopener noreferrer">
-              <BsGithub className="w-6 h-6 text-gray-400 hover:text-gray-500" />
+              <BsGithub className="w-6 h-6 text-orange-600 hover:text-orange-400" />
             </a>
             <a href="#" target="_blank" rel="noopener noreferrer">
-              <BsLinkedin className="w-6 h-6 text-gray-400 hover:text-gray-500" />
+              <BsLinkedin className="w-6 h-6 text-orange-600 hover:text-orange-400" />
             </a>
           </div>
           <p className="mt-8 text-base text-center text-gray-500">
